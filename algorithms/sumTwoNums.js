98725900bd5d9a39e36cdc1f3af4a3faf236e7d5
Benjamin Lee <benjaminlee0727@gmail.com>
2022-11-07 16:23:25 -0600

    added comments

--- algorithms/sumTwoNums.js
+++ algorithms/sumTwoNums.js
@@ -1,9 +1,12 @@
 function twoNumberSum(array, targetSum) {
   for (let i = 0; i < array.length; i++) {
+    //grab current
     let current = array[i];
+    //grab next in arr
     for (let j = i + 1; j < array.length; j++) {
       if (array[j] + current === targetSum) return [current, array[j]];
     }
   }
+  //return empty if no solution
   return [];
 }
