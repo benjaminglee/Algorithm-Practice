de5d54fe58700a3a2632f725c3854e8a224e407d
Benjamin Lee <benjaminlee0727@gmail.com>
2022-12-14 22:10:44 -0600

    add prompt

--- algorithms/easy/studentHeights.js
+++ algorithms/easy/studentHeights.js
@@ -1,3 +1,6 @@
+//two input arrays of equal lengths with positive integers
+//return true if one array is contains integers that are strictly greater than opposing array at same index
+
 function classPhotos(redShirtHeights, blueShirtHeights) {
   const sortedReds = redShirtHeights.sort((a, b) => a - b);
   const sortedBlues = blueShirtHeights.sort((a, b) => a - b);
