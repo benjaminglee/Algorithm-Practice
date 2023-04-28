b1448f2002d255a5c142a63b2a916e17cfcd1b8b
Benjamin Lee <benjaminlee0727@gmail.com>
2023-04-28 18:00:59 -0500

    optimized array of product solution

--- algorithms/medium/arrayOfProducts.js
+++ algorithms/medium/arrayOfProducts.js
@@ -1,13 +1,34 @@
+//O(n^2) time -- brute force solution
+// function arrayOfProducts(array) {
+//     let output = [];
+//     for (let i = 0; i < array.length; i++) {
+//       let product = 1
+//       for (let j = 0; j < array.length; j++) {
+//         if(j !== i) {
+//           product*=array[j] 
+//         }
+//       }
+//       output.push(product)
+//     }
+//     return output
+//   }
+
+//optimized for time and space
+
 function arrayOfProducts(array) {
-    let output = [];
-    for (let i = 0; i < array.length; i++) {
-      let product = 1
-      for (let j = 0; j < array.length; j++) {
-        if(j !== i) {
-          product*=array[j] 
-        }
-      }
-      output.push(product)
+    //crea
+    const products = new Array(array.length).fill(1);
+  
+    let leftProduct = 1;
+    for(let i =0; i < array.length; i++) {
+      products[i] = leftProduct;
+      leftProduct*=array[i];
     }
-    return output
+  
+    let rightProduct =1;
+    for(let i = array.length - 1; i >=0; i--) {
+      products[i] *= rightProduct;
+      rightProduct *= array[i];
+    }
+    return products;
   }
